1082da8f33f8c9ee3971e09f3cc9d6d07344a377
torokv@vnet.hu <Kapitany>
2023-08-22 22:34:34 +0200

    Add project files.

---
+++ F01/Program.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace F01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CsvHelper teszt");
+
+            try
+            {
+                using var reader = new StreamReader("data.csv");
+                using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                var records = csv.GetRecords<Szemely>();
+
+                foreach (var item in records)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ F01/Szemely.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F01
+{
+    public class Szemely
+    {
+        public int Id { get; set; }
+
+        public string Vezeteknev { get; set; }
+
+        public string Keresztnev { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{Id} {Vezeteknev} {Keresztnev}";
+        }
+    }
+}
