db5f7322c5f7b076fe84394c3b952097ff7d69a3
torokv@vnet.hu <Kapitany>
2023-08-25 20:52:00 +0200

    Az F06 feladat megoldása

---
+++ F05/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO.Compression;
+
+namespace F05
+{
+    internal class Program
+    {
+        static void ZipCurrentDirectory()
+        {
+            Console.WriteLine("Zipping current directory...");
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string zipFileName = $"{currentDirectory}.zip";
+
+            Console.WriteLine($"{currentDirectory} -> {zipFileName}");
+
+            ZipFile.CreateFromDirectory(currentDirectory, zipFileName);
+
+            Console.WriteLine("Ready");
+        }
+
+        static void ZipAFile(string fileName)
+        {
+            // TODO: egy adott file tömörítése
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zipper application");
+
+            try
+            {
+                if (args.Length == 0)
+                {
+                    ZipCurrentDirectory();
+                }
+                else
+                {
+                    ZipAFile(args[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ F06/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace F06
+{
+    internal class Program
+    {
+        public static async Task<string> GetChuckNorrisJoke()
+        {
+            // API: https://api.chucknorris.io/
+
+            using (var client = new HttpClient())
+            {
+                return await client.GetStringAsync(@"https://api.chucknorris.io/jokes/random");
+            }
+        }
+
+        public static async void PrintJoke()
+        {
+            string response = await GetChuckNorrisJoke();
+
+            /*Console.WriteLine(response);
+            Console.WriteLine();*/
+
+            if (response != null)
+            {
+                var jsonData = JsonConvert.DeserializeObject(response) as JObject;
+                Console.WriteLine(jsonData?["value"]);
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintJoke();
+
+            Console.WriteLine("Waiting for response...");
+            Console.ReadLine();
+        }
+    }
+}
